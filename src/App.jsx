f7405aab06c22fc7840234dc31d563f338fa0c9c
Noe <adegboyenoah@gmail.com>
2025-11-11 19:50:49 +0100

    installing react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-black text-white font-mono">
+        <p className="text-xl">hello react</p>
+      </div>
+    </>
+  )
+}
+
+export default App
