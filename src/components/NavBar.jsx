44465ec3ae2fb1e1b9672e1d27ac01eb64765b3d
Noe <adegboyenoah@gmail.com>
2025-11-15 09:23:03 +0100

    structuring the navigation and the header UI

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
 const NavBar = () => {
   return (
     <>
-      <div className="navbar h-[50px] bg-[#003580] flex justify-center ">
+      <div className="navbar h-[50px] bg-nav-bg flex justify-center ">
         <div className="nav-container w-full max-w-[1024px] text-white flex items-center justify-between">
           <span className="logo font-bold ">Bookings</span>
           <div className="nav-items">
-            <button className="nav-button ml-[20px] border-none py-[5px] px-[10px] bg-white text-[#003580]">Register</button>
-            <button className="nav-button ml-[20px] border-none py-[5px] px-[10px] bg-white text-[#003580]">Log In</button>
+            <button className="nav-button ml-[20px] border-none py-[5px] px-[10px] bg-white text-nav-bg">Register</button>
+            <button className="nav-button ml-[20px] border-none py-[5px] px-[10px] bg-white text-nav-bg">Log In</button>
           </div>
         </div>
       </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,36 +1,69 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBed,
+  faCalendar,
+  faCar,
+  faHotel,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
     <>
-      <div className="header">
-        <div className="header-list">
-          <div className="header-list-item">
-            <FontAwesomeIcon icon={faBed} />
-            <span>Stays</span>
-          </div>
-          
-          <div className="header-list-item">
-            <FontAwesomeIcon icon={faPlane} />
-            <span>Flights</span>
+      <div className="header bg-nav-bg text-white flex justify-center ">
+        <div className="header-container w-full max-w-[1024px] mx-0 mt-[20px] mb-[100px]">
+          <div className="header-list flex gap-[40px] mb-[50px]">
+            <div className="header-list-item flex items-center gap-[10px]">
+              <FontAwesomeIcon icon={faBed} />
+              <span>Stays</span>
+            </div>
+
+            <div className="header-list-item flex items-center gap-[10px]">
+              <FontAwesomeIcon icon={faPlane} />
+              <span>Flights</span>
+            </div>
+
+            <div className="header-list-item flex items-center gap-[10px]">
+              <FontAwesomeIcon icon={faCar} />
+              <span>Car Rentsls</span>
+            </div>
+
+            <div className="header-list-item flex items-center gap-[10px]">
+              <FontAwesomeIcon icon={faBed} />
+              <span>Attractions</span>
+            </div>
+
+            <div className="header-list-item flex items-center gap-[10px]">
+              <FontAwesomeIcon icon={faTaxi} />
+              <span>Airport Taxis</span>
+            </div>
           </div>
 
-          <div className="header-list-item">
-            <FontAwesomeIcon icon={faCar} />
-            <span>Car Rentsls</span>
-          </div>
+          <h1 className="header-title text-4xl font-bold">A lifetime of discounts? It's Genius.</h1>
+          <p className="header-description my-[20px] mx-0 ">
+            Get rewarded for your travels - unlock instant savings of 10% or
+            more with a free Bookings account
+          </p>
+          <button className="header-button border-none py-[5px] px-[10px] text-white bg-[#0071c2] font-500">Sign In/Register</button>
 
-          <div className="header-list-item">
-            <FontAwesomeIcon icon={faBed} />
-            <span>Attractions</span>
-          </div>
+          <div className="header-search h-[30px] bg-white border-[3px] border-yellow-500 outline-none flex items-center justify-around text-gray-900 ">
+            <div className="header-search-item ">
+                <FontAwesomeIcon icon={faHotel} className="header-icon"/>
+                <input type="text" placeholder="Where are you going?" className="header-search-input" />
+            </div>
+            
+            <div className="header-search-item">
+                <FontAwesomeIcon icon={faCalendar} className="header-icon"/>
+                <span className="header-search-text">date to date</span>
+            </div>
 
-          <div className="header-list-item">
-            <FontAwesomeIcon icon={faTaxi} />
-            <span>Airport Taxis</span>
-          </div>
+            <div className="header-search-item">
+                <FontAwesomeIcon icon={faBed} className="header-icon"/>
+                <span className="header-search-text">2 adults 2 children 1 room </span>
+            </div>
 
+          </div>
         </div>
       </div>
     </>
