3fe0a5980ef82468662700b5fb337929d907231d
Noe <adegboyenoah@gmail.com>
2025-11-20 19:14:45 +0100

    adding all the other pages: the list of hootel page and the individual hotel page, not just the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import List from "./pages/List";
+import Hotel from "./pages/Hotel"
+
 
 function App() {
   return (
@@ -7,6 +10,8 @@ function App() {
       <main className="main">
         <Routes>
           <Route path="/" element={<Home/>} />
+          <Route path="/hotels" element={<List/>} />
+          <Route path="/hotels/:id" element={<Hotel/>} />
         </Routes>
       </main>
     </>
