2e266cbd860bb91949b6e5520599ebddf7c1fbc3
Noe <adegboyenoah@gmail.com>
2025-11-20 19:50:04 +0100

    me ensuring all the added pages work from the get go

---
+++ src/pages/Hotel.jsx
@@ -0,0 +1,12 @@
+
+
+const Hotel = () => {
+
+    return(
+        <>
+            <h1>This is the page for a certain hotel</h1>
+        </>
+    )
+}
+
+export default Hotel
---
+++ src/pages/List.jsx
@@ -0,0 +1,12 @@
+
+
+const List = () => {
+
+    return(
+        <>
+            <h1>A comprehensive list of available hotels</h1>
+        </>
+    )
+}
+
+export default List
